b84d11c326bd3d188509f20b1ea1d51fd43ccbad
Mario Pedisic <mario.pedisic@gmail.com>
2024-06-28 10:52:59 +0200

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+    <title>Web Site</title>
+</head>
+
+<body>
+    <p>prva stranica</p>
+</body>
+
+</html>
